fc3d07285791ca47c9ba8e1783a0951e3e4313b3
MrKSM <KSM>
2018-07-20 11:00:30 +1000

    Added adding function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Simple Hello World String
 var HWorld = "Hello World"
 console.log(HWorld)
+
+// Hello World with a integer attached
+var numbers = 5 + 10
+newHWorld = HWorld + " " + numbers
+console.log(newHWorld)
